af0ae1292a8688b91e8f34ec7544378d86462614
Eikens <m.a.eikens@student.tue.nl>
2022-01-15 16:01:05 +0100

    Update TheHeistVisibilityAlgorithm.cs

--- unity/Assets/Scripts/TheHeist/Controller/TheHeistVisibilityAlgorithm.cs
+++ unity/Assets/Scripts/TheHeist/Controller/TheHeistVisibilityAlgorithm.cs
@@ -10,7 +10,7 @@ using System.Linq;
 using System;
 using Util.Algorithms.Polygon;
 
-public class TheHeistAlgorithmV3 : MonoBehaviour {
+public class TheHeistVisibilityAlgorithm : MonoBehaviour {
 
 	// position of the player,guard and objects.
 	[SerializeField]
@@ -21,17 +21,70 @@ public class TheHeistAlgorithmV3 : MonoBehaviour {
 	Vector2 guardOrientation;
     [SerializeField]
     Ray guardDir;
-    [SerializeField]
-	float coneWidth = 40;
-	[SerializeField]
-	float coneLength;
-    
+ //   [SerializeField]
+	//float coneWidth = 40;
+	//[SerializeField]
+	//float coneLength;
 
-    float CalcAngle(Vector2 vertice)
+    private static Vector2 origin = Vector2.zero;
+    private static float coneWidth = 30;
+
+    private Cone cone;
+
+    // constructor for the class
+    public Polygon2D VisionCone(Polygon2DWithHoles polygon, Vector2 guardPos, Vector2 playerPos, Vector2 guardOrientation)
     {
-        float angle = Vector2.SignedAngle(vertice, Vector2.up);
+        // set values
+        this.playerPos = playerPos;
+        this.guardPos = guardPos;
+        this.guardOrientation = guardOrientation;
+
+        this.cone = new Cone(origin, guardOrientation, coneWidth);
+
+        // shift such that guard is origin of polygon
+        polygon.ShiftToOrigin(guardPos);
+        
+        // create and filter the events
+        List<Event> events = GetEvents(polygon);
+
+        
+        // create sweepline
+        Ray2D sweepline = new Ray2D(origin, Rotate(cone.ray.direction, -90));
+
+        // init status and list for resulting polygon
+        AATree<Segment> status = InitStatusTree(events, sweepline); //new AATree<StatusItem>();
+
+        List<Vector2> result = HandleEvents(events, status, sweepline);
+
+
+        // return the visiblity polygon
+        return new Polygon2D(result);
+    }
+
+    // returns angle between v and cone direction in degrees
+    public float CalcAngle(Vector2 vertice)
+    {
+        float angle = Vector2.SignedAngle(vertice, cone.ray.direction);
         if (angle < 0) angle += 360;
         return angle;
+    }
+
+    private bool betweenAngles(float test, float a1, float a2)
+    {
+        if (a1 < test && test < a2) return true;
+        else return true;
+    }
+
+    public static Vector2 Rotate(Vector2 v, float degrees)
+    {
+        float sin = Mathf.Sin(degrees * Mathf.Deg2Rad);
+        float cos = Mathf.Cos(degrees * Mathf.Deg2Rad);
+
+        float tx = v.x;
+        float ty = v.y;
+        v.x = (cos * tx) - (sin * ty);
+        v.y = (sin * tx) + (cos * ty);
+        return v;
     }
 
     List<Vector2> RadialSweepAlgorithm(Polygon2DWithHoles polygon, List<Vector2> vertices)
@@ -41,7 +94,7 @@ public class TheHeistAlgorithmV3 : MonoBehaviour {
         foreach (var v in vertices)
         {
             // if angle falls outside 180 vision
-            if (CalcAngle(v) > 180) continue;
+            // if (CalcAngle(v) > 180) continue;
 
             visible.Add(v);
         }
@@ -70,78 +123,99 @@ public class TheHeistAlgorithmV3 : MonoBehaviour {
         
     }
 
-    private bool betweenAngles(float test, float a1, float a2)
+    private List<Event> CreateEvent(List<LineSegment> segments, bool isHole)
     {
-        if (a1 < test && test < a2) return true;
-        else return true;
-    }
-
-    List<Event> GetEvents(Polygon2DWithHoles polygon)
-    {
-        Vector2 origin = Vector2.zero;
-        Vector2 direction = new Vector2(0, -1);
-        Vector2 offset = new Vector2(1, 0);
-        Ray2D coneFirst = new Ray2D(origin, direction - offset);
-        Ray2D coneSecond = new Ray2D(origin, direction + offset);
-
-        List<Event> events = new List<Event>();
-        List<LineSegment> segments = polygon.Segments.ToList();
-
-        // create events
-        for (int i = 0; i < segments.Count; i++) {
+        List<Event> e = new List<Event>();
+        for (int i = 0; i < segments.Count; i++)
+        {
             Vector2 vertex = segments[i].Point1;
             LineSegment prev = (i != 0) ? segments[i - 1] : segments[segments.Count - 1];
             LineSegment next = segments[i];
 
-            events.Add(new Event(vertex, new Segment(prev), new Segment(next)));
+            e.Add(new Event(vertex, new Segment(prev), new Segment(next), isHole, cone.ray.direction));
+        }
+        return e;
+    }
+
+    private List<Event> GetEvents(Polygon2DWithHoles polygon)
+    {
+        // Vector2 origin = Vector2.zero;
+        // Vector2 direction = new Vector2(0, -1);
+        // Vector2 offset = new Vector2(1, 0);
+       // Ray2D coneFirst = new Ray2D(origin, direction - offset);
+        // Ray2D coneSecond = new Ray2D(origin, direction + offset);
+
+        List<Event> events = new List<Event>();
+        List<LineSegment> segmentsOutside = polygon.Outside.Segments.ToList();
+        ICollection<Polygon2D> polyHoles = polygon.Holes;
+
+        
+
+        // create outside events
+        events.AddRange(CreateEvent(segmentsOutside, false));
+
+        // add hole events
+        foreach (var hole in polyHoles)
+        {
+            List<LineSegment> segments = hole.Segments.ToList();
+            events.AddRange(CreateEvent(segments, true));
         }
 
         // filter and sort events
-        events.Sort();
-        var angle_cone1 = CalcAngle(coneFirst.direction);
-        var angle_cone2 = CalcAngle(coneSecond.direction);
+        //events.Sort();
+        //var angle_cone1 = CalcAngle(cone.start.direction);
+        //var angle_cone2 = CalcAngle(cone.end.direction);
 
-        foreach (Event e in events) {
-            var angle_v = CalcAngle(e.vertex);
-            var angle_prev = CalcAngle(e.prevSeg.segment.Point1);
-            var angle_next = CalcAngle(e.nextSeg.segment.Point2);
+        //foreach (Event e in events) {
+        //    var angle_v = CalcAngle(e.vertex);
+        //    var angle_prev = CalcAngle(e.prevSeg.segment.Point1);
+        //    var angle_next = CalcAngle(e.nextSeg.segment.Point2);
 
-            // if all points fall outside and on the same side of the cone
-            if (betweenAngles(angle_v, angle_cone1,  270) && betweenAngles(angle_prev, angle_cone1, 270) && betweenAngles(angle_prev, angle_cone1, 270))
-            {
-                events.Remove(e);
-            }
+        //    // if all points fall outside and on the same side of the cone
+        //    if (betweenAngles(angle_v, angle_cone1,  270) && betweenAngles(angle_prev, angle_cone1, 270) && betweenAngles(angle_prev, angle_cone1, 270))
+        //    {
+        //        events.Remove(e);
+        //    }
 
-            if (betweenAngles(angle_v, 90, angle_cone2) && betweenAngles(angle_prev, 90, angle_cone2) && betweenAngles(angle_prev, 90, angle_cone2))
-            {
-                events.Remove(e);
-            }
-        }
+        //    if (betweenAngles(angle_v, 90, angle_cone2) && betweenAngles(angle_prev, 90, angle_cone2) && betweenAngles(angle_prev, 90, angle_cone2))
+        //    {
+        //        events.Remove(e);
+        //    }
+        //}
 
 
         return events;
     }
 
-    void PostProcess()
+    // Initializes the status structure with all Segments intersecting the sweepline
+    private AATree<Segment> InitStatusTree(List<Event> events, Ray2D sweepline)
     {
-
+        AATree<Segment> status = new AATree<Segment>();
+        
+        // if the event's previous segment intersects the sweepline, add the segment to the status
+        foreach ( var e in events)
+        {
+            var intersection = e.prevSeg.segment.Intersect(sweepline);
+            if (intersection.HasValue) status.Insert(e.prevSeg);
     }
+
+        return status;
+    }
+
+    private List<Vector2> HandleEvents(List<Event> events, AATree<Segment> status, Ray2D sweepline)
+    {
+        // List of vertices for the resulting visibility polygon
+        List<Vector2> result = new List<Vector2>();
+
+
+
+        return result;
+    }
+
 
 	void Start()
 	{
 		print("Start");
-
-        //setupLevel(playerPos, guardPos, guardOrientation, coneWidth, coneLength, lines);
-
-        //checkPlayerVisibility(playerPos, guardPos, guardOrientation);
-
-        //List<Line> lines = new List<Line>();
-        //lines.Add(new Line(new Vector2(1f, 1f), new Vector2(3f, 1f)));
-        //lines.Add(new Line(new Vector2(4f, 1f), new Vector2(5f, 1f)));
-        //lines.Add(new Line(new Vector2(-1f, 1f), new Vector2(-3f, 1f)));
-        //lines.Add(new Line(new Vector2(-4f, 1f), new Vector2(-6f, 1f)));
-        //lines.Add(new Line(new Vector2(-1f, 1f), new Vector2(1f, 1f)));
-        //CurrentLevel = lines;
 
 
         List<Vector2> Outer = new List<Vector2>() { 
@@ -154,12 +228,9 @@ public class TheHeistAlgorithmV3 : MonoBehaviour {
         guardPos = new Vector2(1, 1);
         //guardDir = new Ray(guardPos, );
         playerPos = new Vector2(2, 2);
+        guardOrientation = new Vector2(0, 1);
 
-        // shift such that guard is origin of polygon
-        polygon.ShiftToOrigin(guardPos);
-
-        List<Event> events = GetEvents(polygon);
-
+        Polygon2D visiblePoly = VisionCone(polygon, guardPos, playerPos, guardOrientation);
 
         //foreach (var v in polygon.Vertices)
         //{
@@ -194,20 +265,58 @@ public class TheHeistAlgorithmV3 : MonoBehaviour {
 
     }
 
+    private class Cone
+    {
+        public Ray2D ray;
+        public Ray2D start;
+        public Ray2D end;
+
+        public Cone(Vector2 origin, Vector2 direction, float width)
+        {
+            this.ray = new Ray2D(origin, direction);
+
+            Vector2 vecStart = Rotate(direction, width);
+            Vector2 vecEnd = Rotate(direction, -width);
+
+            this.start = new Ray2D(origin, vecStart);
+            this.end = new Ray2D(origin, vecEnd);
+        }
+
+        public Cone(Ray2D direction, float width)
+        {
+            this.ray = direction;
+
+            Vector2 vecStart = Rotate(ray.direction, width);
+            Vector2 vecEnd = Rotate(ray.direction, -width);
+
+            this.start = new Ray2D(origin, vecStart);
+            this.end = new Ray2D(origin, vecEnd);
+        }
+
+
+    }
+
     // Event consists of a point and its adjecent line segments
     public class Event : IComparable<Event>, IEquatable<Event>
     {
         public readonly Vector2 vertex;
+        // public float degrees;
+        public float radians;
         public Segment prevSeg;
         public Segment nextSeg;
+        public bool isHole;
 
         private static readonly VertexComparer vc = new VertexComparer();
 
-        public Event(Vector2 v, Segment prevSeg, Segment nextSeg)
+        public Event(Vector2 vertex, Segment prevSeg, Segment nextSeg, bool isHole, Vector2 coneDirection)
         {
-            vertex = v;
+            this.vertex = vertex;
             this.prevSeg = prevSeg;
             this.nextSeg = nextSeg;
+            this.isHole = isHole;
+
+           // this.degrees = CalcAngle(Vector2.zero);
+            this.radians = (float) MathUtil.Angle(vertex, origin, coneDirection);
         }
 
         public int CompareTo(Event otherEvent)
